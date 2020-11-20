ace2a625a5fcd3b044e0b53f3fb24f44a7bb5a3e
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-20 08:57:58 -0800

    add API call

--- src/js/call-exchange-rate.js
+++ src/js/call-exchange-rate.js
@@ -1,7 +1,13 @@
 export default class ExchangeRate {
   static async getExchangeRate() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      if (!respons.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
     }
   }
 }
