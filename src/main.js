242c9265737f5a24bbcc6dccefdabc2a0befcff9
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-20 10:02:52 -0800

    add callApi function and start HTML

--- src/main.js
+++ src/main.js
@@ -5,7 +5,16 @@ import './css/styles.css';
 import ConvertCurrency from './js/currency-conversion.js'; 
 import ExchangeRate from './js/call-exchange-rate.js'
 
+getElements(response) {
+  if (response.conversion_rates){
+    //response.conversion_rates.JPY => 103.9219
+  }
+}
 
+async function makeApiCall() {
+  const response = await ExchangeRate.getExchangeRate();
+  getElements(response);
+}
 
 $(document).ready(function() {
 
