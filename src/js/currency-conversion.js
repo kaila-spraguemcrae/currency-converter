8c31969b3432d83273c67ad050820b0bf678d006
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-20 15:14:21 -0800

    add structure styling, cards and row

--- src/js/currency-conversion.js
+++ src/js/currency-conversion.js
@@ -1,3 +1,3 @@
-export default function convert(amount, start, end) {
-  return Math.round(((amount / start)*end)*100)/100;
+export default function convert(start, amount, end) {
+  return Math.round(((amount / start)* end)*100)/100;
 }
--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import './css/styles.css';
 import convert from './js/currency-conversion.js'; 
 import ExchangeRate from './js/call-exchange-rate.js';
 
-function getElements(response, inputtedAmount, inputtedCurrency) {
+function getElements(response, inputtedStart, inputtedAmount, inputtedEnd) {
   if (response.conversion_rates){
-    let conversionRate = parseFloat(response.conversion_rates[inputtedCurrency]);
-    let newAmount = convert(inputtedAmount,conversionRate);
+    let conversionRateEnd = parseFloat(response.conversion_rates[inputtedEnd]);
+    let conversionRateStart = parseFloat(response.conversion_rates[inputtedStart]);
+    let newAmount = convert(conversionRateStart, inputtedAmount,conversionRateEnd);
+    console.log(conversionRateEnd);
+    console.log(conversionRateStart);
     $('#results').html(`Currency converted: ${newAmount}`);
     console.log(newAmount);
   } else {
@@ -17,25 +20,25 @@ function getElements(response, inputtedAmount, inputtedCurrency) {
   }
 }
 
-async function makeApiCall(inputtedAmount,inputtedCurrency) {
+async function makeApiCall(inputtedStart, inputtedAmount,inputtedEnd) {
   const response = await ExchangeRate.getExchangeRate();
-  getElements(response, inputtedAmount, inputtedCurrency);
+  getElements(response, inputtedStart, inputtedAmount, inputtedEnd);
 }
 
 $(document).ready(function() {
   $('#user-input').submit(function(event){
     event.preventDefault();
-
+    
+    const inputtedStart = $('#start-currency').val();
     const inputtedAmount = parseFloat($('#amount').val());
-    const inputtedCurrency = $('#end-currency').val();
+    const inputtedEnd = $('#end-currency').val();
 
     if (!inputtedAmount > 0) {
       alert("Please enter a number greater than 0");
-    } else if (inputtedCurrency == "") {
+    } else if (inputtedEnd == "") {
       alert("Please select a currency");
     } else {
-      makeApiCall(inputtedAmount, inputtedCurrency);
+      makeApiCall(inputtedStart, inputtedAmount, inputtedEnd);
     }
   });
-
 });
