3d2d17fb2be8e1987c2d18efd3533b4d447d58b4
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-20 08:52:43 -0800

    update webpack to process .env

---
+++ src/js/call-exchange-rate.js
@@ -0,0 +1,7 @@
+export default class ExchangeRate {
+  static async getExchangeRate() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    }
+  }
+}
