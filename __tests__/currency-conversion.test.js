060d190422b4afe12c91f5e3232ba6d17b1d2774
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-20 09:11:48 -0800

    add test 1

---
+++ __tests__/currency-conversion.test.js
@@ -0,0 +1,8 @@
+import convertCurrency from './../src/js/currency-conversion.js'
+
+describe( 'convertCurrency', () => {
+
+test('Should convert inputted USD currency into selected currency')
+  expect(10,JPY).toEqual(1038.10);
+
+});
