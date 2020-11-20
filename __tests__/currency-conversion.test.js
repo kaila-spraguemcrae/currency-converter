b662c3cc4c0d6b3816d5b131a2ceb78b47e3c925
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-20 11:15:36 -0800

    pass refactored test 1

--- __tests__/currency-conversion.test.js
+++ __tests__/currency-conversion.test.js
@@ -12,6 +12,6 @@ describe( 'convert', () => {
 
   test('Should convert inputted USD currency into selected currency', () => {
     let JPY = 103.9219;
-    expect(convert(10,JPY)).toEqual(1079.22);
+    expect(convert(10,JPY)).toEqual(1039.22);
   });
 });
